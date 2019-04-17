1c6408f835607a661b537a662f762a33507ccf2e
John Carney <john@pdfta.com>
2019-04-17 20:47:50 +0100

    Added code to load the model.

--- app/main.py
+++ app/main.py
@@ -1,25 +1,9 @@
 from flask import Flask, jsonify
-# from google.oauth2 import service_account
-from google.cloud import storage
-import json
+from google.oauth2 import service_account
+import model
 
 
 APP = Flask(__name__)
-
-
-def download_from_cloud_storage(credentials):
-    client = storage.Client(project=credentials.project_id,
-                            credentials=credentials)
-    bucket = client.get_bucket('iris-model')
-    blob = bucket.get_blob('iris-model/lte_fe34789_20190318141552947255.pkl')
-
-    blob.download_to_filename('/models/model.pkl')
-
-
-def load_model():
-    global model
-    with open('/models/model.pkl', 'rb') as f:
-        model = json.load(f)
 
 
 @APP.route("/prediction", methods=['GET', 'POST'])
@@ -28,7 +12,7 @@ def prediction():
 
 
 if __name__ == "__main__":
-    # credentials = service_account.Credentials.from_service_account_file(
-    #     '/secrets/private-key.json')
-    # download_from_cloud_storage(credentials)
+    credentials = service_account.Credentials.from_service_account_file(
+        '/secrets/private-key.json')
+    model.download_from_cloud_storage(credentials)
     APP.run(host='0.0.0.0', port=5000, debug=True)
